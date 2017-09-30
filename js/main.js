42fd996d3a9cb18e956d468a888fee723b71e010
Alexey Bachurin <bachura95@gmail.com>
2017-10-01 02:54:27 +0300

    Сайт без адаптива

--- js/main.js
+++ js/main.js
@@ -1,4 +1,50 @@
 $(document).ready(function() {
 
+	// fancybox
+	$('[data-fancybox]').fancybox({
+		autoSize: true,
+		protect: true
+	});
+
+	// Accordion
+	$('.q_title').on("click", function(){
+
+		if($(this).hasClass('active')){
+			$(this).removeClass("active");
+			$(this).next('.answer').slideUp(200);
+		} else {
+			$('.q_title').removeClass("active");
+			$(this).addClass("active");
+			$('.answer').slideUp(200);
+			$(this).next('.answer').slideDown(200);
+		}
+	})
+
+	// formstyler.min.js 
+	  $('input[type="radio"], input[type="checkbox"], input[type="file"], select').styler();
+
+	// Pop_up form
+	$(".mymagicoverbox").click(function()
+	{
+				$('#myfond_gris').fadeIn(300);
+				var iddiv = $(this).attr("iddiv");
+				$('#'+iddiv).fadeIn(300);
+				$('#myfond_gris').attr('opendiv',iddiv);
+				return false;
+	});
+
+	$('#modal_close, #dribble_modal_close, #myfond_gris, .mymagicoverbox_fermer').click(function()
+	{
+				var iddiv = $("#myfond_gris").attr('opendiv');
+				$('#myfond_gris').fadeOut(300);
+				$('#'+iddiv).fadeOut(300);
+	});
+
+	// viewport_checker
+	 AOS.init({
+	 	disable: 'mobile',
+    easing: 'ease',
+    delay: 50,
+	 });
 
 }); 
